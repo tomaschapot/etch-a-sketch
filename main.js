d3e65c3fefd37f0a434b8fca54125d8b187f952e
Tomas <53576581+tomaschapot@users.noreply.github.com>
2022-02-27 14:48:47 -0300

    added color selection

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const $container = document.querySelector(".container");
 const $button = document.querySelector("button");
+let $colorList = document.querySelector("#colors");
 let boxSize;
 
 //Start Button
@@ -11,6 +12,19 @@ $button.addEventListener("click", () => {
 	const $gridDensity = document.querySelector("#grid-density").value;
 	createBoxes($gridDensity);
 });
+
+function currentColor(colorList) {
+	return colorList.value;
+}
+
+function removeAllColor(event) {
+	event.target.classList.remove(`painted-red`);
+	event.target.classList.remove(`painted-blue`);
+	event.target.classList.remove(`painted-yellow`);
+	event.target.classList.remove(`painted-green`);
+	event.target.classList.remove(`painted-black`);
+	event.target.classList.remove(`painted-orange`);
+}
 
 function createBoxes(divisons) {
 	boxSize = 900 / divisons;
@@ -25,26 +39,31 @@ function createBoxes(divisons) {
 }
 
 $container.addEventListener("mouseover", (e) => {
+	let color = currentColor($colorList);
+
 	if (e.ctrlKey === true) {
-		e.target.classList.add("painted");
+		removeAllColor(e);
+		e.target.classList.add(`painted-${color}`);
 		event.stopPropagation(e);
 	}
 	if (e.ctrlKey === true && e.altKey === true) {
-		e.target.classList.remove("painted");
+		removeAllColor(e);
+
 		event.stopPropagation(e);
 	}
 });
 
 $container.addEventListener("click", (e) => {
+	let color = currentColor($colorList);
+	removeAllColor(e);
 	if (e.ctrlKey === false && e.target.classList[0] === "box") {
-		e.target.classList.add("painted");
+		e.target.classList.add(`painted-${color}`);
 		event.stopPropagation(e);
 	}
 	if (e.altKey === true) {
-		e.target.classList.remove("painted");
+		removeAllColor(e);
 		event.stopPropagation(e);
 	}
-	console.log(e);
 });
 
 createBoxes(16); //starting grid
